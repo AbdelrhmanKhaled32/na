9f4ba4054a48c8746de604d4eb89455f8fa8cd36
abdelrhman khaled <abdelrahmankhaled581@gmail.com>
2023-12-05 02:53:37 +0200

    first project

---
+++ area/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Dice
+{
+    private readonly int sides;
+
+    public Dice(int sides)
+    {
+        this.sides = sides;
+    }
+
+    public int GetRoll()
+    {
+        Random rand = new Random();
+        return rand.Next(1, sides + 1);
+    }
+}
